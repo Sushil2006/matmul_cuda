e259049aac465cc13d19725ebe0295c7650e3f9b
Sushil <sushilraaja@gmail.com>
2026-09-07 17:45:03 +0530

    Bank conflict free version

--- benchmark.cu
+++ benchmark.cu
@@ -12,6 +12,7 @@
 void launch_naive(const float *A, const float *B, float *C, int M, int N, int K);
 void launchSmemTiled(const float *A, const float *B, float *C, int M, int N, int K, int BM, int BN, int BK);
 void launchBlockTiled(const float *A, const float *B, float *C, int M, int N, int K, int BM, int BN, int BK, int TM, int TN);
+void launchBankConflictFree(const float *A, const float *B, float *C, int M, int N, int K, int BM, int BN, int BK, int TM, int TN);
 
 static void check(cudaError_t error)
 {
@@ -42,7 +43,7 @@ static bool parse_positive(const char *text, int &value)
 static int usage(const char *program)
 {
     std::cerr << "usage: " << program << " --M <positive> --N <positive> --K <positive>"
-              << " [--kernel naive|smem|block] [--BM <positive> --BN <positive> --BK <positive>]"
+              << " [--kernel naive|smem|block|bank-free] [--BM <positive> --BN <positive> --BK <positive>]"
               << " [--TM <positive> --TN <positive>] [--runs <positive>] [--no-verify]\n";
     return 1;
 }
@@ -87,8 +88,10 @@ int main(int argc, char **argv)
     const bool use_naive = !std::strcmp(kernel, "naive");
     const bool use_smem = !std::strcmp(kernel, "smem");
     const bool use_block = !std::strcmp(kernel, "block");
-    if (M == 0 || N == 0 || K == 0 || (!use_naive && !use_smem && !use_block) ||
-        ((use_smem || use_block) && (BM == 0 || BN == 0 || BK == 0)) || (use_block && (TM == 0 || TN == 0)))
+    const bool use_bank_free = !std::strcmp(kernel, "bank-free");
+    const bool use_thread_tiled = use_block || use_bank_free;
+    if (M == 0 || N == 0 || K == 0 || (!use_naive && !use_smem && !use_thread_tiled) ||
+        ((use_smem || use_thread_tiled) && (BM == 0 || BN == 0 || BK == 0)) || (use_thread_tiled && (TM == 0 || TN == 0)))
         return usage(argv[0]);
 
     const size_t a_count = static_cast<size_t>(M) * K;
@@ -121,6 +124,8 @@ int main(int argc, char **argv)
         launchSmemTiled(d_A, d_B, d_C, M, N, K, BM, BN, BK);
     else if (use_block)
         launchBlockTiled(d_A, d_B, d_C, M, N, K, BM, BN, BK, TM, TN);
+    else if (use_bank_free)
+        launchBankConflictFree(d_A, d_B, d_C, M, N, K, BM, BN, BK, TM, TN);
     else
         launch_naive(d_A, d_B, d_C, M, N, K);
     check(cudaGetLastError());
@@ -136,6 +141,8 @@ int main(int argc, char **argv)
             launchSmemTiled(d_A, d_B, d_C, M, N, K, BM, BN, BK);
         else if (use_block)
             launchBlockTiled(d_A, d_B, d_C, M, N, K, BM, BN, BK, TM, TN);
+        else if (use_bank_free)
+            launchBankConflictFree(d_A, d_B, d_C, M, N, K, BM, BN, BK, TM, TN);
         else
             launch_naive(d_A, d_B, d_C, M, N, K);
     }
